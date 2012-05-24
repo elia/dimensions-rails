5248b32bd54dbe80be3a4d549ff292573898905e
Elia Schito <elia@schito.me>
2012-05-24 18:56:19 +0200

    Add the ONE spec

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def index
+    render :inline => "<%= image_tag('rails.png') %>"
+  end
 end
---
+++ spec/helper/dimensions-rails_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe ApplicationHelper, type: :helper do
+  it 'adds size to images' do
+    tag = helper.image_tag('rails.png')
+    tag.should match('height="64"')
+    tag.should match('width="50"')
+  end
+end
