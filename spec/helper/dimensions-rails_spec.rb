fdbdbef6924d06c20da5278775bb7fdd47ca2c84
Elia Schito <elia@schito.me>
2012-05-25 00:31:37 +0200

    Support 1.8.7 syntax
    Specs on 1.8.7 fails as described in issue #1, 
    confirming that has been fixed.

--- spec/helper/dimensions-rails_spec.rb
+++ spec/helper/dimensions-rails_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ApplicationHelper, type: :helper do
+describe ApplicationHelper, :type => :helper do
   it 'adds size to images' do
     tag = helper.image_tag('rails.png')
     tag.should match('height="64"')
