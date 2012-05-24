3629fb23a9b978b1371b380b13f028dd27a4300f
Elia Schito <elia@schito.me>
2012-05-24 18:49:15 +0200

    Setup Rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+Rails.backtrace_cleaner.remove_silencers!
+
+# require 'rails/test_help'
+require 'rspec/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+end
