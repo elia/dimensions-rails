309b8167052effec24c0b413960715747d17977d
Jeroen van Ingen <jeroeningen@gmail.com>
2012-06-15 23:59:29 +0200

    Error fixed when image is missing

--- spec/helper/dimensions-rails_spec.rb
+++ spec/helper/dimensions-rails_spec.rb
@@ -6,4 +6,10 @@ describe ApplicationHelper, :type => :helper do
     tag.should match('height="64"')
     tag.should match('width="50"')
   end
+  
+  it "should not return no image or error if the image is missing" do
+    tag = helper.image_tag('missing.png')
+    tag.should match('height="0"')
+    tag.should match('width="0"')
+  end
 end
